6e60787cc2017e6838b0f8fc65fc2c8022b3667b
kaustavvats <kaustav16048@iiitd.ac.in>
2020-03-04 21:32:38 +0530

    Q1b Done

--- Assignment-2/Q1b.py
+++ Assignment-2/Q1b.py
@@ -13,14 +13,7 @@ def checkVariable(expr):
     for i in range(len(expr)):
         if expr[i] != '(' and expr[i] != ')' and expr[i] != '+' and expr[i] != '.' and expr[i] != '*' and expr[i] != '==' and expr[i] != '~':
             varSet.add(expr[i])
-    print(varSet)
-    return len(varSet), varSet
-
-def singleF(a, varValues, varMap):
-    if len(a) == 2:
-        if type(a[1]) != bool:
-            return not varValues[varMap[a[1]]]
-        return not
+    return len(varSet), sorted(list(varSet))
 
 def andF(a1, a3, varValues, varMap):
     if type(a1) != bool:
@@ -44,39 +37,75 @@ def impliesF(a1, a3, varValues, varMap):
 
 def biimplies(a1, a3, varvarValues, varMapMap):
     return impliesF(a1, a3, varvarValues, varMapMap) and impliesF(a3, a1, varvarValues, varMapMap)
-    
 
-def evaluate(varMap, expr, varValues):
+def findExpr(listExpr):
+    for i in range(len(listExpr)):
+        if listExpr[i] == '+' or listExpr[i] == '.' or listExpr[i] == '*' or listExpr[i] == '==':
+            return i
+    return None
+
+def handleNegation(listVar, varValues, varMap):
+    if listVar[0] != '~':
+        print("[Error] Negation")
+        return None
+    if type(listVar[1]) != bool:
+        r = not (varValues[varMap[listVar[1]]])
+    else:
+        r = not (listVar[1])
+    return r
+
+def handleSingleVar(listVar, varValues, varMap):
+    if type(listVar[0]) != bool:
+        r = varValues[varMap[listVar[0]]]
+    else:
+        r = listVar[0]
+    return r
+
+def evaluate(varMap, expr, varValues, pl=False):
     stack = []
     for i in range(len(expr)):
         if expr[i] == ')':
-            oneF = ""
-            while(stack[-1] != '('):
-                oneF = stack.pop() + oneF
+            temp = []
+            while(len(stack) >= 0 and stack[-1] != '('):
+                temp.insert(0, stack.pop())
+            if pl:
+                print(temp)
             stack.pop()
-            if len(oneF) > 2:
-                if oneF.find('+'):
-                    a1, a3 = oneF.split('+')
-                elif oneF.find('.'):
-                    a1, a3 = oneF.split('.')
-                elif oneF.find('*'):
-                    a1, a3 = oneF.split('*')
-                else:
-                    a1, a3 = oneF.split('==')
+            if len(temp) == 2:
+                res = handleNegation(temp, varValues, varMap)
+            elif len(temp) == 1:
+                res = handleSingleVar(temp, varValues, varMap)
             else:
-                
-            # a3 = stack.pop()
-            # a2 = stack.pop()
-            # a1 = stack.pop()
-            # stack.pop()
-            # if a2 == '+':
-            #     res = andF(a1, a3, varValues, varMap)
-            # elif a2 == '.':
-            #     res = orF(a1, a3, varValues, varMap)
-            # elif a2 == '.':
-            #     res = orF(a1, a3, varValues, varMap)
-            # elif a2 == '.':
-            #     res = orF(a1, a3, varValues, varMap)
+                index = findExpr(temp)
+                ex1 = []
+                ex2 = []
+                for i in range(0, index):
+                    ex1.append(temp[i])
+                for i in range(index+1, len(temp)):
+                    ex2.append(temp[i])
+
+                if len(ex1) == 2:
+                    p1 = handleNegation(ex1, varValues, varMap)
+                elif len(ex1) == 1:
+                    p1 = handleSingleVar(ex1, varValues, varMap)
+
+                if len(ex2) == 2:
+                    p2 = handleNegation(ex2, varValues, varMap)
+                elif len(ex2) == 1:
+                    p2 = handleSingleVar(ex2, varValues, varMap)
+                if pl:
+                    print(p1, p2)
+                if temp[index] == '+':
+                    res = orF(p1, p2, varValues, varMap)
+                elif temp[index] == '.':
+                    res = andF(p1, p2, varValues, varMap)
+                elif temp[index] == '*':
+                    res = impliesF(p1, p2, varValues, varMap)
+                else:
+                    res = biimplies(p1, p2, varValues, varMap)
+            if pl:
+                print(res)
+                print()
             stack.append(res)
         elif expr[i] == '~':
             if stack[-1] == '~':
@@ -98,7 +127,11 @@ def createTruthTable(truthTable, expr):
     results = [False for i in range(len(truthTable))]
     varMap = getVarMap(expr)
     for i in range(len(truthTable)):
-        results[i] = evaluate(varMap, expr, truthTable[i])
+        if (i == 1):
+            results[i] = evaluate(varMap, expr, truthTable[i], pl=True)
+        else:
+            results[i] = evaluate(varMap, expr, truthTable[i])
+        print("------------------------------------\n")
     showTruthTable(truthTable, results, varMap)
 
 def showTruthTable(truthTable, results, varMap):
@@ -109,15 +142,38 @@ def showTruthTable(truthTable, results, varMap):
         for j in range(len(truthTable[i])):
             print(truthTable[i][j], end='\t')
         print(results[i])
+    print("\nDisjunctive Normal Form:", disjunctionNormalForm(truthTable, results, varMap))
+
+def disjunctionNormalForm(tTable, result, varMap):
+    Ans = []
+    for i in range(len(result)):
+        if result[i]:
+            r = []
+            for j, e in enumerate(varMap.keys()):
+                if tTable[i][j]:
+                    r.append(e)
+                else:
+                    r.append("~" + e)
+            s = ".".join(r)
+            s = "(" + s + ")"
+            Ans.append(s)
+    dnf = " + ".join(Ans)
+    if len(Ans) > 1:
+        dnf = "(" + dnf + ")"
+    return dnf
 
 if __name__ == "__main__":
     
     # expr = str(input("Enter Formula: "))
-    expr = "((A + B) *  C) * ((A * C) . (B * C))"
+    # expr = "(((A . B) *  C) * ((A * C) + (B * C)))"
+    # expr = "(~A . ~B)"
+    expr = "((A + ~B) . ~C)"
+    # expr = "(A)"
     expr = re.sub(r"\s+", '', expr)
     # print(expr)
     varCount, _ = checkVariable(expr)
     # print(TruthTableSize)
-    TruthTable = table = list(itertools.product([True, False], repeat=varCount))
-    # print(TruthTable)
+    table = list(itertools.product([True, False], repeat=varCount))
+    print(table)
+    createTruthTable(table, expr)
 
