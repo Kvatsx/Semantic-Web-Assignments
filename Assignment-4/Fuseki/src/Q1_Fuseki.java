ff480359b4e7a2ba1bedf5e119b40cf8497f2ccb
kaustavvats <kaustav16048@iiitd.ac.in>
2020-05-08 02:42:03 +0530

    Q1 3 Parts Done

--- Assignment-4/Fuseki/src/Q1_Fuseki.java
+++ Assignment-4/Fuseki/src/Q1_Fuseki.java
@@ -32,28 +32,55 @@ public class Q1_Fuseki {
 
     public void moveMovies(RDFConnection conn, String[] graphNames) {
         String query = "" +
-                "PREFIX  xsd:    <http://www.w3.org/2001/XMLSchema#>" +
+                "COPY DEFAULT TO <" + graphNames[0]+">";
+        conn.update(query);
+        query = "" +
+                "COPY DEFAULT TO <" + graphNames[1]+">";
+        conn.update(query);
+        query = "CLEAR DEFAULT";
+        conn.update(query);
+        conn.commit();
+
+        query = "PREFIX  xsd:    <http://www.w3.org/2001/XMLSchema#>" +
                 "PREFIX  node:     <http://www.semanticweb.org/kvats/ontologies/2020/3/Netflix#>" +
-                "SELECT ?nodeObject ?showid ?releasedIn " +
+                "DELETE {" +
+                "  GRAPH <http://iiitd.ac.in/sweb/2016048/newmoviesgraph> {" +
+                "    ?x ?property ?value ;" +
+                "       node:releasedIn ?year ." +
+                "  }" +
+                "}" +
                 "WHERE {" +
-                "  ?nodeObject node:hasShowId ?showid ;" +
-                "  node:releasedIn ?releasedIn . FILTER (?releasedIn > \"2016\"^^xsd:int )" +
-                "} " +
-                "ORDER BY ASC(?releasedIn)";
-        QueryExecution qe = conn.query(query);
-        ResultSet results = qe.execSelect();
-        ResultSetFormatter.out(System.out, results);
-        qe.close();
-        System.out.println(results);
+                "  GRAPH <http://iiitd.ac.in/sweb/2016048/newmoviesgraph> {" +
+                "    ?x ?property ?value ;" +
+                "    node:releasedIn ?year . FILTER( ?year < \"2016\"^^xsd:int)" +
+                "  }" +
+                "}";
+        conn.update(query);
+        query = "PREFIX  xsd:    <http://www.w3.org/2001/XMLSchema#>" +
+                "PREFIX  node:     <http://www.semanticweb.org/kvats/ontologies/2020/3/Netflix#>" +
+                "DELETE {" +
+                "  GRAPH <http://iiitd.ac.in/sweb/2016048/oldmoviesgraph> {" +
+                "    ?x ?property ?value ;" +
+                "       node:releasedIn ?year ." +
+                "  }" +
+                "}" +
+                "WHERE {" +
+                "  GRAPH <http://iiitd.ac.in/sweb/2016048/oldmoviesgraph> {" +
+                "    ?x ?property ?value ;" +
+                "    node:releasedIn ?year . FILTER( ?year >= \"2016\"^^xsd:int)" +
+                "  }" +
+                "}";
+        conn.update(query);
+        conn.commit();
     }
 
     public static void main(String[] args) {
         Q1_Fuseki q1 = new Q1_Fuseki();
         RDFConnection conn = RDFConnectionFactory.connect("http://localhost:3030/Netflix/");
         String[] graphNames = {"http://iiitd.ac.in/sweb/2016048/newmoviesgraph", "http://iiitd.ac.in/sweb/2016048/oldmoviesgraph"};
-//        q1.run(conn);
-//        q1.createGraph(conn, graphNames[0]);
-//        q1.createGraph(conn, graphNames[1]);
+        q1.run(conn);
+        q1.createGraph(conn, graphNames[0]);
+        q1.createGraph(conn, graphNames[1]);
         q1.moveMovies(conn, graphNames);
         conn.close();
     }
